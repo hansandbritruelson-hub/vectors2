22500ec9559f9c779eb8f8dda1a6d5323bac3b1c
Developer <user@example.com>
2026-02-13 07:54:55 -0600

    update styles

--- templates/TestComponent.vue
+++ templates/TestComponent.vue
@@ -6,8 +6,23 @@
 </script>
 
 <template>
-    <div style="height: 100px; width: 200px; background-color: #884444; flex-direction: column;">
-        <text text="I am a component!" style="color: #ffffff;" />
-        <text :text="props.text" style="color: #ffff00;" />
+    <div class="component-container">
+        <text text="I am a component!" class="white-text" />
+        <text :text="props.text" class="yellow-text" />
     </div>
 </template>
+
+<style>
+.component-container {
+    height: 100px;
+    width: 200px;
+    background-color: #884444;
+    flex-direction: column;
+}
+.white-text {
+    color: #ffffff;
+}
+.yellow-text {
+    color: #ffff00;
+}
+</style>
